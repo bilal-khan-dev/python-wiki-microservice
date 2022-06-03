98a5498257449c1c890da1d995aeed47ce46fb95
Bilal Khan <38106396+devbilalkhan@users.noreply.github.com>
2022-06-03 00:24:09 +0000

    Adding phrase func with textblob corpora

--- main.py
+++ main.py
@@ -1,3 +1,38 @@
-from mylib.logic import wiki
+from fastapi import FastAPI 
+import uvicorn
+from mylib.logic import (wiki as wikilogic , search_wiki, phrases as wiki_phrases)
 
-print(wiki())
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Wikipedia API. Call /search or /wiki"}
+
+@app.get("/search/{value}")
+async def search(value: str):
+    """
+    Page to search wikipedia
+    """
+    return {"results": search_wiki(value)}
+
+@app.get("/wiki/{name}")
+async def wiki(name: str):
+    """
+    Retrieve wikipedia page
+    """
+    return {
+        "result": wikilogic(name)
+    }
+
+@app.get("/phrase/{phrase}")
+async def phrase(phrase: str):
+    """
+    Retrieve wikipedia page and returns phrases
+    """
+    return {
+        "result": wiki_phrases(phrase)
+    }
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8080, host='0.0.0.0')
